df63d6cea1aa7822241989f3fc7b599d68dc19f3
Youssef Noujoum <ynoujoum@e3r3p9.1337.ma>
2025-09-23 21:57:28 +0100

    add ex02

--- cpp module 00/ex02/Account.cpp
+++ cpp module 00/ex02/Account.cpp
@@ -1,5 +1,65 @@
 #include <iostream>
 #include <cctype>
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) : _accountIndex(0),	_amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
+
+}
+
+Account::~Account() {
+    
+}
+
+void	_displayTimestamp( void )
+{
+	
+}
+
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+// int		Account::checkAmount( void )
+// {
+
+// }
+
+// void	Account::displayStatus( void )
+// {
+
+// }
+
 
 int main()
 {
