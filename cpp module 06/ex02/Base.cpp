fcaa64e313ce46dac608d68b02d451134dc05b30
Youssef Noujoum <ynoujoum@e3r3p7.1337.ma>
2025-11-16 23:48:35 +0100

    update

--- cpp module 06/ex02/Base.cpp
+++ cpp module 06/ex02/Base.cpp
@@ -24,20 +24,27 @@ void Base::identify(Base* p) {
         std::cout << 'A' << std::endl;
     else if (dynamic_cast<B *>(p))
         std::cout << 'B' << std::endl;
-    else
+    else if (dynamic_cast<C *>(p))
         std::cout << 'C' << std::endl;
+    else
+        std::cout << "unknown" << std::endl;
 };
 
 void Base::identify(Base& p) {
     try {
         A a = dynamic_cast<A&>(p);
         std::cout << 'A' << std::endl;
-    } catch (std::exception &a) {
+    } catch (std::exception &e) {
         try {
-            B a = dynamic_cast<B&>(p);
+            B b = dynamic_cast<B&>(p);
             std::cout << 'B' << std::endl;
-        } catch (std::exception &a) {
-            std::cout << 'C' << std::endl;
+        } catch (std::exception &e) {
+            try {
+                C c = dynamic_cast<C&>(p);
+                std::cout << 'B' << std::endl;
+            } catch (std::exception &e) {
+                std::cout << "unknown" << std::endl;
+            }
         }
     }
 };
