3724d28222e6247dd3214f98aef49615b38d141a
youssefnjm <youssefnoujoum04@gmail.com>
2025-12-24 08:35:26 +0100

    ...

--- cpp module 07/ex02/Array.hpp
+++ cpp module 07/ex02/Array.hpp
@@ -8,50 +8,41 @@
 
 template <typename T> class Array {
     public:
-        T *a;
+        T *arr;
         size_t size;
 
         Array() {
-            a = new T[0];
+            size = 0;
+            arr = new T[0];
         }
         Array(unsigned int n) {
             size = n;
-            a = new T[size];
+            arr = new T[size];
         }
-        Array(const Array &array) {
-            this->a = NULL;
-            *this = array;
+        Array(const Array &other) {
+            this->arr = NULL;
+            *this = other;
         }
-        Array operator =(const Array &array) {
-            if (this != &array)
+        Array& operator =(const Array &other) {
+            if (this != &other)
             {
-                if (this->a != NULL)
-                    delete[] this->a;
-                this->a = new T[array.size];
+                delete[] this->arr;
 
-                std::cout << "here " << std::endl;
-                for (size_t i = 0; i < array.size; i++) {
+                this->size = other.size;
+                this->arr = new T[size];
 
-                    this->a[i] = array.a[i];
+                for (size_t i = 0; i < this->size; i++) {
+                    this->arr[i] = other.arr[i];
                 }
             }
             return *this;
         }
-        
+
         T& operator[](size_t idx) {
-            if (idx < 0 || idx >= this->size)
-                throw std::out_of_range ("blah");;
-            return this->a[idx];
+            if (idx >= this->size)
+                throw std::out_of_range ("indx out of range");
+            return this->arr[idx];
         }
-
-        // T& operator[](size_t idx) const {
-        //     if (idx >= this->size)
-        //         throw std::out_of_range ("blah");;
-        //     return this->a[idx];
-        // }
-
 };
 
-
 #endif
-
--- cpp module 07/ex02/main.cpp
+++ cpp module 07/ex02/main.cpp
@@ -2,7 +2,7 @@
 #include <cstddef>
 #include <iostream>
 
-#define MAX_VAL 2
+#define MAX_VAL 10
 
 int main(int, char**)
 {
@@ -20,35 +20,30 @@ int main(int, char**)
     //SCOPE
     {
         Array<int> tmp = numbers;
-        std::cout << "here" << std::endl;
         Array<int> test(tmp);
     }
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     if (mirror[i] != numbers[i])
-    //     {
-    //         std::cerr << "didn't save the same value!!" << std::endl;
-    //         return 1;
-    //     }
-    // }
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
 
-    // try{
-    //     numbers[-2] = 0;
-    // } catch(const std::exception& e) {
-    //     std::cerr << e.what() << '\n';
-    // }
+    try{
+        numbers[-2] = 0;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
 
-    // try {
-    //     numbers[MAX_VAL] = 0;
-    // } catch(const std::exception& e) {
-    //     std::cerr << e.what() << '\n';
-    // }
+    try {
+        numbers[MAX_VAL] = 0;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     numbers[i] = rand();
-    // }
 
     delete [] mirror;//
     return 0;
