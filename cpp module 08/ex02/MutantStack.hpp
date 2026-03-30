e27ca267e4678a4b1adbe6d2cb695c93b92056ee
youssefnjm <youssefnoujoum04@gmail.com>
2026-03-30 14:15:42 +0100

    update

--- cpp module 08/ex02/MutantStack.hpp
+++ cpp module 08/ex02/MutantStack.hpp
@@ -6,26 +6,26 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
     public:
-        MutantStack() {}
+        MutantStack() {};
         MutantStack(const MutantStack<T> &other) {
             *this = other;
-        }
+        };
         MutantStack &operator=(const MutantStack<T> &other) {
             if (this != &other) {
                 std::stack<T>::operator=(other);
             }
             return *this;
-        }
-        ~MutantStack() {}
+        };
+        ~MutantStack() {};
 
         typedef typename std::stack<T>::container_type::iterator iterator;
 
         iterator begin(void) {
             return this->c.begin();
-        }
+        };
         iterator end(void) {
             return this->c.end();
-        }
+        };
 
 };
 
