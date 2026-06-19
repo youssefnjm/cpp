a56035f8ab1a7a614bc73c476bd6dc4cf696f951
youssefnjm <youssefnoujoum04@gmail.com>
2026-06-19 17:18:46 +0100

    improbe cpp09/ex01

--- cpp module 09/ex01/RPN.cpp
+++ cpp module 09/ex01/RPN.cpp
@@ -2,59 +2,65 @@
 #include <iostream>
 #include <sstream>
 #include <stack>
+#include <stdexcept>
 
 RPN::RPN(void) {}
 RPN::RPN(const RPN &other) { (void) other; }
 RPN &RPN::operator=(const RPN &other) { (void) other; return (*this); }
 RPN::~RPN(void) {}
 
+bool isInputValid(char c) {
+    return (!(c >= '0' && c <= '9') 
+        && !(c == '+' || c == '-' || c == '*' || c == '/'));
+};
+
 void RPN::calculate(std::string input)
 {
-    std::stringstream ss(input);
     std::stack<int> stack;
 
+    std::stringstream ss(input);
     std::string buffer;
     while (ss >> buffer) {
-        if (buffer.length() != 1) {
-            std::cerr << "Error: argument should be less than 10\n";
-            return ;
-        }
 
-        if (!(buffer[0] >= '0' && buffer[0] <= '9')
-            && !(buffer[0] == '+' || buffer[0] == '-' || buffer[0] == '*' || buffer[0] == '/'))
-        {
-            std::cerr << "Error: argument should be numbers or operator '+ - / *'\n";
-            return ;
-        }
+        if (buffer.length() != 1 || isInputValid(buffer[0]))
+            throw std::runtime_error("Error: argument should be operator (+ - / *) or numbers less than 10.");
 
         if (buffer[0] >= '0' && buffer[0] <= '9') {
             stack.push(std::atoi(buffer.c_str()));
+            continue ;
         }
-        else
-        {
-            if (stack.size() < 2) {
-                std::cerr << "Error: not enough numbers compared to number of operators\n";
-                return;
-            }
-            int num1 = stack.top();
-            stack.pop();
-            int num2 = stack.top();
-            stack.pop();
 
-            if (buffer[0] == '+') {
-                stack.push(num2 + num1);
-            } else if (buffer[0] == '-') {
-                stack.push(num2 - num1);
-            } else if (buffer[0] == '*') {
-                stack.push(num2 * num1);
-            } else {
-                if (num1 == 0) {
-                    std::cerr << "Error: can't div by 0\n";
-                    return;
-                }
-                stack.push(num2 / num1);
-            }
+        if (stack.size() < 2) {
+            throw std::runtime_error("Error: Invalid expression (too many operators left over).");
         }
+
+        int num1 = stack.top();
+        stack.pop();
+        int num2 = stack.top();
+        stack.pop();
+
+        int res;
+        if (buffer[0] == '+') {
+            res = num2 + num1;
+        } 
+        else if (buffer[0] == '-') {
+            res = num2 - num1;
+        } 
+        else if (buffer[0] == '*') {
+            res = num2 * num1;
+        } 
+        else {
+            if (num1 == 0)
+                throw std::runtime_error("Error: can't div by 0");
+
+            res = num2 / num1;
+        }
+
+        stack.push(res);
     }
+
+
+    if (stack.size() != 1)
+        throw std::runtime_error("Error: Invalid expression (too many numbers left over).");
     std::cout << stack.top() << std::endl;
 }
--- cpp module 09/ex01/main.cpp
+++ cpp module 09/ex01/main.cpp
@@ -1,13 +1,17 @@
 #include "RPN.hpp"
-#include <iostream>
 
 int main(int ac, char **av) {
-    if (ac == 1) {
-        std::cerr << "Error\n";
+
+    try {
+        if (ac == 1)
+            throw std::runtime_error("Error: Unvalid argument numbers.");
+
+        RPN::calculate(av[1]);
+
+    } catch (std::exception &err) {
+        std::cerr << err.what() << std::endl;
         return 1;
     }
-
-    RPN::calculate(av[1]);
 
     return 0;
 }
